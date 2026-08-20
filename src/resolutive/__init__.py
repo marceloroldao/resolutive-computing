b71b8474b03f553a50260b495a3ef4ddd235a15a
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-20 10:42:07 -0300

    feat(api): expose optimize as package entry point

--- src/resolutive/__init__.py
+++ src/resolutive/__init__.py
@@ -1,6 +1,13 @@
-"""Resolutive Computing research package."""
+"""Resolutive Computing public package interface."""
 
+from .api import optimize
+from .optimization.common import OptimizationResult
 from .optimization.v2 import ResolutiveV2
 from .optimization.v5 import ResolutiveV5
 
-__all__ = ["ResolutiveV2", "ResolutiveV5"]
+__all__ = [
+    "optimize",
+    "OptimizationResult",
+    "ResolutiveV2",
+    "ResolutiveV5",
+]
