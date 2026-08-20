a11f5890ade7bcff4f0729dd9bc74d3641882baa
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-20 11:51:47 -0300

    feat(api): expose ask-tell session

--- src/resolutive/__init__.py
+++ src/resolutive/__init__.py
@@ -4,10 +4,13 @@ from .api import optimize
 from .optimization.common import OptimizationResult
 from .optimization.v2 import ResolutiveV2
 from .optimization.v5 import ResolutiveV5
+from .session import AskBatch, OptimizationSession
 
 __all__ = [
     "optimize",
     "OptimizationResult",
+    "OptimizationSession",
+    "AskBatch",
     "ResolutiveV2",
     "ResolutiveV5",
 ]
