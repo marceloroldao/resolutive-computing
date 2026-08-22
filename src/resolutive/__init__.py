621dfab7b3f27481dd21fae43ffb0fd30751dce1
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-22 06:34:34 -0300

    feat(api): expose session checkpoint helpers

--- src/resolutive/__init__.py
+++ src/resolutive/__init__.py
@@ -1,6 +1,7 @@
 """Resolutive Computing public package interface."""
 
 from .api import create_session, optimize
+from .checkpoint import checkpoint_json, checkpoint_session, restore_json, restore_session
 from .multires_session import MultiResolutionSession, MultiResolutionState
 from .optimization.common import OptimizationResult
 from .optimization.v2 import ResolutiveV2
@@ -10,6 +11,10 @@ from .session import AskBatch, OptimizationSession
 __all__ = [
     "optimize",
     "create_session",
+    "checkpoint_session",
+    "restore_session",
+    "checkpoint_json",
+    "restore_json",
     "OptimizationResult",
     "OptimizationSession",
     "MultiResolutionSession",
